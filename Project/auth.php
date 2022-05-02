e84297a241958894ae37fd57694cf430bd9534aa
Stanislav Tiltevskii <tiltevsk@cs.petrsu.ru>
2022-05-02 19:13:52 +0300

    auth

--- Project/auth.php
+++ Project/auth.php
@@ -1,6 +1,8 @@
 <?php
-    $uname = filter_var(trim($_POST['uname']));
-    $psw = filter_var(trim($_POST['psw']));
+    session_start();
+    $uname = filter_var(trim($_POST['uname']), FILTER_SANITIZE_STRING);
+    $psw = filter_var(trim($_POST['psw']), FILTER_SANITIZE_STRING);
+
 
     $servername = "localhost";
     $database = "druzhini";
@@ -8,11 +10,13 @@
     $password = "fa3Aphie";
     // Создаем соединение
     $mysql = mysqli_connect($servername, $username, $password, $database);
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     if (!$mysql) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    echo "Connected successfully<br/>";
-    $result = $mysql->query("SELECT * FROM 'tblAdmin' WHERE 'txtLogin' = '$uname' AND 'txtPassword' = '$psw'");
+    else echo "Connected successfully<br/>";
+    $result = $mysql->query("SELECT * FROM `tblAdmin` WHERE `txtLogin` = '$uname' AND `txtPassword` = '$psw'");
+    
     $user = $result->fetch_assoc();
     if(count($user) == 0)
     {
