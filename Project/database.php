55c698b0fe4dfc096559b19c29cf94765c2d1aaf
SvatLisov <67441837+SvatLisov@users.noreply.github.com>
2022-05-02 15:55:43 +0300

    Update database.php

--- Project/database.php
+++ Project/database.php
@@ -1,8 +1,8 @@
 <?php 
-$par1_ip = "";
-$par2_name = "";
-$par3_pas = "";
-$par4_db = "";
+$par1_ip = "kappa.cs.petrsu.ru";
+$par2_name = "druzhini";
+$par3_pas = "fa3Aphie";
+$par4_db = "databases";
 
 $induction = mysqli_coonect($par1_ip,$par2_name,$par3_pas,$par4_db);
 
@@ -11,3 +11,12 @@ if ($induction == false)
     echo "Хьюстон, у нас проблемы с подключением к БД!";
 }
 ?>
+/*Подключение к БД:
+*1. ssh 'name'@kappa.cs.petrsu.ru
+*2. mysql -u druzhini -p
+*3. Password: fa3Aphie
+*MariaDB:
+*1.show databases;
+*2. use druzhini
+*3. show tables;
+*/
