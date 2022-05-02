9211c9859871867b0f79814ae23db408502d608d
Glazerrr <glazerrr228@gmail.com>
2022-05-02 14:35:10 +0300

    php

---
+++ Project/connect.php
@@ -0,0 +1,9 @@
+<?php
+    $user=$_POST['user']
+    $surname=$_POST['surname']
+    $secname=$_POST['secname']
+    $province=$_POST['province']
+    $date=$_POST['date']
+    $nadzor=$_POST['nadzor']
+
+?>
