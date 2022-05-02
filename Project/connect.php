797270df2140e1a2519354ee3011af16c12b91da
Glazerrr <glazerrr228@gmail.com>
2022-05-02 15:49:23 +0300

    asdf

--- Project/connect.php
+++ Project/connect.php
@@ -1,12 +1,16 @@
-<?php
-    $user=$_POST['user']
-    $surname=$_POST['surname']
-    $secname=$_POST['secname']
-    $province=$_POST['province']
-    $date=$_POST['date']
-    $nadzor=$_POST['nadzor']
-    echo "$user"
-    echo "$secname"
 
-    
+<?php
+    $user=$_POST['user'];
+    $surname=$_POST['surname'];
+    $secname=$_POST['secname'];
+    $province=$_POST['province'];
+    $date=$_POST['date'];
+    $nadzor=$_POST['nadzor'];
+    print "Имя: $user<br/>";
+    print "Фамилия: $surname<br/>";
+    print "Отчество: $secname<br/>";
+    print "Губерния: $province<br/>";
+    print "Когда учрежден надзор: $date<br/>";
+    print "Где учрежден надзор: $nadzor<br/>";
+
 ?>
